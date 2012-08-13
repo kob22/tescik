60fce8cc22342d8abe907bd73accc3a118d90d5e
kob22 <kob2222@gmail.com>
2012-08-13 21:17:55 +0200

    df

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tescik::Application.config.secret_token = 'c3f14514a3084bddd8be5bedc7da9b46ad59f56d8ed35de57dd89474db0ca577db490bcb751344082d122e329a94b8723c530d454ed886452bb6bcd5f77ac3ce'
